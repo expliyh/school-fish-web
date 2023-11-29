131115c94c1beebc7d8a6be2d9a428e2a02bf566
Expliyh <expliyh@outlook.com>
2023-11-29 09:15:47 +0800

    添加登录页面

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,78 @@
 <script setup lang="ts">
+import {defineComponent, ref} from 'vue';
+import {ElMessage} from "element-plus";
 
+
+// name: 'Login',
+
+const loginForm = ref({
+  username: '',
+  password: '',
+});
+
+const handleSubmit = () => {
+  // 这里可以进行用户名和密码的校验，然后执行登录逻辑
+  if (!loginForm.value.username || !loginForm.value.password) {
+    ElMessage.error('用户名和密码不能为空！')
+    return;
+  }
+  alert('登录成功！');
+  // 执行登录操作，例如发起网络请求等
+};
+
+const mouse_over_login = () => {
+  if (!loginForm.value.username || !loginForm.value.password) {
+    ElMessage.error('Oops, this is a error message.')
+  }
+}
 </script>
 
 <template>
-
+  <h2 class="form-header">登录</h2>
+  <div class="input-container">
+    <el-input
+        v-model="loginForm.username"
+        placeholder="用户名"
+        autocomplete="off"
+        size="large"
+        @keyup.enter="handleSubmit"
+        clearable
+    ></el-input>
+  </div>
+  <div class="input-container">
+    <el-input
+        v-model="loginForm.password"
+        placeholder="密码"
+        show-password
+        autocomplete="off"
+        @keyup.enter="handleSubmit"
+        size="large"
+        clearable
+    ></el-input>
+  </div>
+  <div class="button-container">
+    <el-button class="form-button">注册</el-button>
+    <el-button class="form-button" type="primary" @click="handleSubmit">登录</el-button>
+  </div>
 </template>
 
 <style scoped>
+.form-header {
+  margin-bottom: 35px;
+}
 
+.input-container {
+  margin-bottom: 35px;
+  width: 90%;
+}
+
+.button-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.form-button {
+  max-width: 45%;
+}
 </style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import Login from "@/components/Login.vue";
+</script>
+
+<template>
+  <el-row justify="center" align="middle" style="height: 100%;margin: 0;padding: 0">
+    <el-col class="login-container" style="align-items: center;" :xs="18" :sm="12" :md="8" :lg="6" :xl="4">
+      <Login></Login>
+    </el-col>
+  </el-row>
+</template>
+
+<style scoped>
+.login-container {
+  padding: 25px;
+  border-radius: 4px;
+  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  background-color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
